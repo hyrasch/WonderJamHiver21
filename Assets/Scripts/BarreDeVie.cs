dbba522ac556f562b97cb56b4f7f08ae44895a06
hyrasch <sylvain.franco@outlook.fr>
2021-02-27 17:14:40 -0500

    CrushTheKingV0.1

--- Assets/Scripts/BarreDeVie.cs
+++ Assets/Scripts/BarreDeVie.cs
@@ -7,22 +7,31 @@ public class BarreDeVie : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public GameObject Runner;
     private GameObject GoBarre { get; set; }
     private float vie;
-    public float Vie 
+    public float vieValue 
     { 
         get { return vie; }
         set {
             if (value > 1f) value = 1f;
             else if (value < 0f) value = 0f;
             vie = value;
-            GoBarre.GetComponent<Image>().fillAmount = value;
+            GoBarre.GetComponent<Image>().fillAmount = vie;
         }
     }
 
     void Start()
     {
         GoBarre = gameObject.transform.GetChild(2).gameObject;
-        Vie = 1f;
+        vieValue = Runner.gameObject.GetComponent<Character2DController>().health;
+        vie = vieValue;
+
+    }
+
+    private void Update()
+    {
+        vieValue = Runner.gameObject.GetComponent<Character2DController>().health;
+        vie = vieValue;
     }
 }
