5e1e85662a757768fdd99fcb3dbaf726754c7057
hyrasch <sylvain.franco@outlook.fr>
2021-02-26 19:44:21 -0500

    Changes Scrolling
    BG doesn't disappear anymore

--- Assets/Scripts/repeatBG.cs
+++ Assets/Scripts/repeatBG.cs
@@ -6,14 +6,40 @@ public class repeatBG : MonoBehaviour
 {
     public float lenght;
 
-    public Transform playerTransform;
+    private float posBG1;
+    private float posBG2;
+
+
+    private void Start()
+    {
+        posBG1 = -1.34f;
+        posBG2 = 11.18f;
+    }
 
     private void Update()
     {
-        if(playerTransform.position.y - gameObject.transform.position.y > lenght)
+        if(transform.position.y > posBG1)
         {
-            Vector3 pos = new Vector3(transform.position.x, transform.position.y + lenght * 2, transform.position.z);
-            transform.position = pos;
+            var gameObject = new GameObject();
+            var pos = new Vector3(-4.64f, posBG1 + 2*lenght, 4);
+            gameObject.transform.localScale = new Vector3(1.575756f, 1.575756f, 1.575756f);
+            gameObject.transform.position = pos;
+            var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+            var sprite = Resources.Load<Sprite>("castleBG");
+            spriteRenderer.sprite = sprite;
+            posBG1 += 2 * lenght;
+        }
+
+        if (transform.position.y > posBG2)
+        {
+            var gameObject = new GameObject();
+            var pos = new Vector3(-4.64f, posBG2 + 2*lenght, 4);
+            gameObject.transform.localScale = new Vector3(1.575756f, 1.575756f, 1.575756f);
+            gameObject.transform.position = pos;
+            var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+            var sprite = Resources.Load<Sprite>("castleBG");
+            spriteRenderer.sprite = sprite;
+            posBG2 +=  2 *lenght;
         }
     }
 }
