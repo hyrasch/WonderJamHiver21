7257dae179065721e75c43a722f1acd971067b15
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-02-26 23:39:44 -0600

    Tetris logic

--- Assets/Scripts/BlockSelection.cs
+++ Assets/Scripts/BlockSelection.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class BlockSelection : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> block;
+    [SerializeField] private List<GameObject> blocks;
+    [SerializeField] private Transform parent;
+    [SerializeField] private float timeBetweenDrop = 2f;
+
+    private GameObject _block;
+    private bool _canSelect = true;
     
     // Start is called before the first frame update
     private void Start()
@@ -15,6 +20,55 @@ public class BlockSelection : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        GetNextBlock();
         
+        if (_block == null) return;
+        
+        MoveBlock();
+        DropBlock();
+    }
+    
+    private void GetNextBlock()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space) || !_canSelect  || _block != null) return;
+        
+        _block = Instantiate(RandomBlock(), parent);
+        _canSelect = false;
+    }
+
+    private void MoveBlock()
+    {
+        var blockTransform = _block.transform;
+        if (Input.GetKey(KeyCode.A))
+        {
+            blockTransform.position += new Vector3(-0.01f, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            blockTransform.position += new Vector3(0.01f, 0f, 0f);
+        }
+    }
+
+    private void DropBlock()
+    {
+        if (!Input.GetKeyDown(KeyCode.Return)) return;
+        
+        _block.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        _block = null;
+
+        StartCoroutine(UpdateDropTimer());
+    }
+
+    private GameObject RandomBlock()
+    {
+        var index = Random.Range(0, blocks.Count);
+        return blocks[index];
+    }
+
+    private IEnumerator UpdateDropTimer()
+    {
+        yield return new WaitForSeconds(timeBetweenDrop);
+
+        _canSelect = true;
     }
 }
