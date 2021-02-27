94275cb9254d58c763995a2fe329b043e2981fec
Timothee <timothee.durgeaud@gmail.com>
2021-02-27 00:43:43 -0500

    Begun Effect logic

--- Assets/Scripts/TetrisBlock.cs
+++ Assets/Scripts/TetrisBlock.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
+
+    TetrisBlockStaticValue.BlockEffect _AttachedEffect;
+    SpriteRenderer _SpriteComponent;
+
     [SerializeField]
-    Vector3 _InitialPosition;
-    [SerializeField]
-    Vector3 _InitialRotation;
-
-    Sprite _SpriteComponent;
-
-
+    TetrisBlockStaticValue _StaticAttribute;
 
     // Start is called before the first frame update
     void Start()
     {
-        _SpriteComponent = GetComponent<Sprite>();
-        
+        _SpriteComponent = GetComponent<SpriteRenderer>();
+        _AttachedEffect = TetrisBlockStaticValue.BlockEffect.Neutral;
     }
 
     void RotateRight() {
@@ -32,12 +30,20 @@ public class TetrisBlock : MonoBehaviour
     void Update()
     {}
 
-    public void SpawnInGameWorld(Vector3 Position, Vector3 Rotation, Vector3 Scale) {
-        _InitialPosition = Position;
-        _InitialRotation = Rotation;
 
-        transform.position = Position;
-        transform.rotation = Quaternion.Euler(Rotation);
-        transform.localScale = Scale;
+    TetrisBlockStaticValue.BlockEffect chooseRandomEffect() {
+        List<int> population = new List<int>();
+        for (int i = 0; i < _StaticAttribute._effectProbabilty.Count; i++) {
+            for (int j = 0; j < 100 * _StaticAttribute._effectProbabilty[i]; j++) {
+                population.Add(i);
+            }
+        }
+        return (TetrisBlockStaticValue.BlockEffect)population[Random.Range(0, _StaticAttribute._effectProbabilty.Count)];
+    }
+
+
+    public void SpawnInGameWorld() {
+        _AttachedEffect = chooseRandomEffect();
+        _SpriteComponent.color = _StaticAttribute._effectColor[(int)_AttachedEffect];
     }
 }
