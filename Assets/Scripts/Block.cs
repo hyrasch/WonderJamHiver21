a87b010a88506b3cd46f5aa0c569c11ecfed4e0b
Pierre <pierresourdeau@hotmail.fr>
2021-02-26 22:30:48 -0500

    Spawn aléatoire des blocs

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBound;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -speed);
+        screenBound = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //a enlever quand y aura collision entre les blocks
+        if(transform.position.y<-2*screenBound.y)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/spawnBlock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class spawnBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> blocksPrefabs; //contient les prefabs des différents blocs
+    public float respawnTime = 1.0f;
+    private Vector2 screenBound;
+    public float pixelPerUnit = 32f;
+    void Start()
+    {
+        screenBound = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(waveBlocks());
+    }
+
+    private void spawnARandomBlock()
+    {
+        System.Random rnd = new System.Random();
+        int index = rnd.Next(blocksPrefabs.Count);
+        GameObject block = Instantiate(blocksPrefabs[index]) as GameObject;
+
+        //Random position sur x, dans le futur faire en sorte que ça soit dans une colonne qui spawn
+        float positionX = Random.Range(-screenBound.x * 0.9f, screenBound.x * 0.9f);
+
+        block.transform.position = new Vector2(positionX, screenBound.y * 1.5f);
+
+        //random de la rotation
+        int rotationFactor = rnd.Next(-3, 3);
+        block.transform.Rotate(new Vector3(0, 0, 1), rotationFactor * 90);
+        //block.transform.localScale.Scale(new Vector3(0.01f, 0.01f, 0.01f));
+        //block.transform.localScale.Scale(new Vector3(0.15f, 0.15f, 1));
+    }
+
+   IEnumerator waveBlocks()
+    {
+        while(true) // à changer en while(inGame)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnARandomBlock();
+        }
+
+    }
+
+
+}
