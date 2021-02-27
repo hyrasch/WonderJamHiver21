9e1d55477919bee5a6c3267e53b76b12503fddf7
Oscar Trosseau <57917670+OscarAFK@users.noreply.github.com>
2021-02-26 23:13:47 -0500

    Première version de l'interface

---
+++ Assets/Scripts/BarreDeVie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarreDeVie : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject GoBarre { get; set; }
+    private float vie;
+    public float Vie 
+    { 
+        get { return vie; }
+        set {
+            if (value > 1f) value = 1f;
+            else if (value < 0f) value = 0f;
+            vie = value;
+            GoBarre.GetComponent<Image>().fillAmount = value;
+        }
+    }
+
+    void Start()
+    {
+        GoBarre = gameObject.transform.GetChild(2).gameObject;
+        Vie = 1f;
+    }
+}
---
+++ Assets/Scripts/TimerAndScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerAndScore : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI timer;
+
+    [SerializeField]
+    TextMeshProUGUI score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void setScore(int value)
+    {
+        score.SetText(value.ToString());
+    }
+
+    void setTimer(string value)
+    {
+        timer.SetText(value);
+    }
+
+}
