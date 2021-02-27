36da34787033da94fcb415003b6ced65ec6693ad
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-02-26 22:22:53 -0600

    Prefabs edition

---
+++ Assets/Scripts/BlockSelection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSelection : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> block;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
