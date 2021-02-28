0e8c87e05cf69a176d7f8b6042b9df5d0629cb21
Nypsyy <mayeul.marsaut@hotmail.fr>
2021-02-28 04:02:48 +0100

    Pause menu + score button (does nothing)

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseUI;
+
+    private Player _master;
+    private Player _runner;
+
+    private void Awake() {
+        _master = ReInput.players.GetPlayer("Master");
+        _runner = ReInput.players.GetPlayer("Runner");
+    }
+
+    private void Update() {
+        if (_master.GetButtonDown("Pause")) {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0f;
+            UpdateControllerMaps("Menu", "Menu");
+            _runner.isPlaying = false;
+        }
+
+        if (_runner.GetButtonDown("Pause")) {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0f;
+            UpdateControllerMaps("Menu", "Menu");
+            _master.isPlaying = false;
+        }
+    }
+
+    public void UpdateControllerMaps(string masterMapName, string runnerMapName) {
+        var masterMap = _master.controllers.maps.mapEnabler;
+        var runnerMap = _runner.controllers.maps.mapEnabler;
+
+        foreach (var ruleSet in masterMap.ruleSets)
+            ruleSet.enabled = false;
+
+        foreach (var ruleSet in runnerMap.ruleSets)
+            ruleSet.enabled = false;
+
+        masterMap.ruleSets.Find(rs => rs.tag == masterMapName).enabled = true;
+        runnerMap.ruleSets.Find(rs => rs.tag == runnerMapName).enabled = true;
+
+        masterMap.Apply();
+        runnerMap.Apply();
+    }
+
+    public void Continue() {
+        pauseUI.SetActive(false);
+        UpdateControllerMaps("Master", "Runner");
+        Time.timeScale = 1f;
+        _master.isPlaying = _runner.isPlaying = true;
+    }
+
+    public void MainMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
