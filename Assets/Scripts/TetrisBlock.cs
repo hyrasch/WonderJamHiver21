0cbcff7b65247e253baca81ecd0eb3cdf6dc7dd9
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2021-02-27 18:01:53 -0600

    Fix merge

--- Assets/Scripts/TetrisBlock.cs
+++ Assets/Scripts/TetrisBlock.cs
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-=======
-﻿using System.Collections.Generic;
 using System.Collections;
->>>>>>> c6e772cdb721fb33bb9c9ac96630d5b7d43fdf15
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 public class TetrisBlock : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] private TetrisBlockStaticValue staticAttribute;
     [SerializeField] private float wheelSpeed = .005f;
     [SerializeField] private int nbWheelRotation = 25;
@@ -21,37 +14,19 @@ public class TetrisBlock : MonoBehaviour
     private TetrisBlockStaticValue.BlockEffect _attachedEffect;
     private SpriteRenderer _spriteComponent;
     private SpriteRenderer _wheel;
-=======
-    TetrisBlockStaticValue.BlockEffect _AttachedEffect;
-    SpriteRenderer _SpriteComponent;
-
-    [SerializeField] TetrisBlockStaticValue _StaticAttribute;
-    
     public GameObject enemyPrefab;
->>>>>>> c6e772cdb721fb33bb9c9ac96630d5b7d43fdf15
 
     private void Start()
     {
         InitAttr();
     }
 
-    private void FixedUpdate()
-    {
-    }
-
     private void InitAttr()
     {
-<<<<<<< HEAD
         if (_spriteComponent != null) return;
         
         _spriteComponent = GetComponent<SpriteRenderer>();
         _attachedEffect = TetrisBlockStaticValue.BlockEffect.Neutral;
-=======
-        if (_SpriteComponent != null) return;
-
-        _SpriteComponent = GetComponent<SpriteRenderer>();
-        _AttachedEffect = TetrisBlockStaticValue.BlockEffect.Neutral;
->>>>>>> c6e772cdb721fb33bb9c9ac96630d5b7d43fdf15
     }
 
     public void RotateRight()
@@ -64,34 +39,24 @@ public class TetrisBlock : MonoBehaviour
         transform.Rotate(0, 0, -90);
     }
 
-<<<<<<< HEAD
-    private TetrisBlockStaticValue.BlockEffect ChooseRandomEffect() {
-        var population = new List<int>();
-        for (var i = 0; i < staticAttribute._effectProbabilty.Count; i++) {
-            for (var j = 0; j < 100 * staticAttribute._effectProbabilty[i]; j++) {
-                population.Add(i);
-            }
-        }
-        return (TetrisBlockStaticValue.BlockEffect) population[Random.Range(0, staticAttribute._effectProbabilty.Count)];
-=======
     private TetrisBlockStaticValue.BlockEffect ChooseRandomEffect()
     {
-        float rand = Random.value;
-        float currentProba = 0;
-        for (var i = 0; i < _StaticAttribute._effectProbabilty.Count; i++){
-            currentProba += _StaticAttribute._effectProbabilty[i];
-            if (rand <= currentProba) {
-                return (TetrisBlockStaticValue.BlockEffect)i;
+        var rand = Random.value;
+        var currentProb = 0f;
+        for (var i = 0; i < staticAttribute._effectProbabilty.Count; i++)
+        {
+            currentProb += staticAttribute._effectProbabilty[i];
+            if (rand <= currentProb) 
+            {
+                return (TetrisBlockStaticValue.BlockEffect) i;
             }
         }
-        return (TetrisBlockStaticValue.BlockEffect)(_StaticAttribute._effectProbabilty.Count - 1);
->>>>>>> c6e772cdb721fb33bb9c9ac96630d5b7d43fdf15
+        return (TetrisBlockStaticValue.BlockEffect) (staticAttribute._effectProbabilty.Count - 1);
     }
 
     public void SpawnInGameWorld()
     {
         InitAttr();
-<<<<<<< HEAD
         
         StartCoroutine(ColorRandomizer());
     }
@@ -137,10 +102,6 @@ public class TetrisBlock : MonoBehaviour
             
             wheelTransform.localPosition = new Vector2(wheelPosition.x, 192 * -1 - (int) _attachedEffect * wheelImgSize);
         }
-=======
-
-        _AttachedEffect = ChooseRandomEffect();
-        _SpriteComponent.color = _StaticAttribute._effectColor[(int) _AttachedEffect];
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -150,65 +111,67 @@ public class TetrisBlock : MonoBehaviour
             gameObject.layer = 8;
         }
 
-        if (gameObject.layer == 8)
+        if (gameObject.layer != 8) return;
+        
+        switch (_attachedEffect)
         {
-            switch (_AttachedEffect)
-            {
-                case TetrisBlockStaticValue.BlockEffect.Neutral:
+            case TetrisBlockStaticValue.BlockEffect.Neutral:
 
-                    break;
-                case TetrisBlockStaticValue.BlockEffect.Fire:
-                    Debug.Log("ça brule");
-                    Fire(collision);
-                    break;
-                case TetrisBlockStaticValue.BlockEffect.Ice:
-                    Debug.Log("Glagla");
-                    Ice();
-                    break;
-                case TetrisBlockStaticValue.BlockEffect.Explosion:
-                    Debug.Log("Tic Tac");
-                    StartCoroutine(WaitForBoom(2.0f));
-                    break;
-                case TetrisBlockStaticValue.BlockEffect.Enemy:
-                    Debug.Log("Be careful");
-                    Enemy();
-                    break;
-                case TetrisBlockStaticValue.BlockEffect.Malus:
-                    Debug.Log("Lucky you!");
-                    Malus();
-                    break;
-            }
-
-            //gameObject.GetComponent<Rigidbody2D>().isKinematic = true; A débattre
+                break;
+            case TetrisBlockStaticValue.BlockEffect.Fire:
+                Debug.Log("ça brule");
+                Fire(collision);
+                break;
+            case TetrisBlockStaticValue.BlockEffect.Ice:
+                Debug.Log("Glagla");
+                Ice();
+                break;
+            case TetrisBlockStaticValue.BlockEffect.Explosion:
+                Debug.Log("Tic Tac");
+                StartCoroutine(WaitForBoom(2.0f));
+                break;
+            case TetrisBlockStaticValue.BlockEffect.Enemy:
+                Debug.Log("Be careful");
+                Enemy();
+                break;
+            case TetrisBlockStaticValue.BlockEffect.Malus:
+                Debug.Log("Lucky you!");
+                Malus();
+                break;
+            default:
+                Debug.LogError("Attached effect missing. Check Tetris Block OnCollisionEnter2D.");
+                break;
         }
+
+        //gameObject.GetComponent<Rigidbody2D>().isKinematic = true; A débattre
     }
 
     private IEnumerator WaitForBoom(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
+        
         Boom();
     }
 
     private void Boom()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2);
+        var colliders = Physics2D.OverlapCircleAll(transform.position, 2);
 
-        foreach (Collider2D hit in colliders)
+        foreach (var hit in colliders)
         {
-            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
-            if (rb != null)
+            var rb = hit.GetComponent<Rigidbody2D>();
+            if (rb == null) continue;
+            
+            if (rb.CompareTag("Block"))
             {
-                if (rb.tag == "Block")
-                {
-                    Destroy(rb.gameObject);
-                }
+                Destroy(rb.gameObject);
             }
         }
     }
 
-    private void Fire(Collision2D collision)
+    private static void Fire(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Character2DController>().health -= 3;
         }
@@ -221,13 +184,14 @@ public class TetrisBlock : MonoBehaviour
 
     private void Enemy()
     {
-        Instantiate(enemyPrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        var transformPosition = transform.position;
+        
+        Instantiate(enemyPrefab, new Vector3(transformPosition.x, transformPosition.y, 0), Quaternion.identity);
         Destroy((gameObject));
     }
 
     private void Malus()
     {
         
->>>>>>> c6e772cdb721fb33bb9c9ac96630d5b7d43fdf15
     }
 }
